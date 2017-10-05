7afd3dba3f61944e1b12d1d5cc4fd31de04e6eea
Namjun Kim <namjunemy@gmail.com>
2017-10-06 04:20:55 +0900

    12장 멀티스레드 5번 문제 풀이

---
+++ ch12_멀티스레드/src/example/Ex05.java
@@ -0,0 +1,14 @@
+package example;
+
+public class Ex05 {
+  /**
+   * 05. 스레드 일시 정지 상태에 대한 설명 중 틀린 것은 무엇입니까?
+   *
+   * 1. 일시 정지 상태는 BLOCKED,WAITING,TIMED_WAITING이 있다.
+   * 2. 스레드 동기화 메소드를 실행할 때 다른 스레드가 동기화 메소드를 호출하게 되면 BLOCKED 일시 정지 상태가 된다.
+   * 3. 동기화 메소드 내에서 wait() 메소드를 호출하면 WAITING 일시 정지 상태가 된다.
+   * 4. yield() 메소드를 호출하면 TIMED_WAITING 일시 정지 상태가 된다.
+   *
+   * 4 -> yield() 메소드를 호출하면 실행 대기 상태가 된다.
+   */
+}
