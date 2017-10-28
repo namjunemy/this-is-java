a19311c126b0ebe9575829fdf3b4b37d7ec256bc
Namjun Kim <namjunemy@gmail.com>
2017-10-29 01:22:56 +0900

    consumer 함수적 인터페이스의 andTher을 이용한 순차적 연결

---
+++ ch14_람다식/src/andthen_compose/Address.java
@@ -0,0 +1,19 @@
+package andthen_compose;
+
+public class Address {
+  private String country;
+  private String city;
+
+  public Address(String country, String city) {
+    this.country = country;
+    this.city = city;
+  }
+
+  public String getCountry() {
+    return country;
+  }
+
+  public String getCity() {
+    return city;
+  }
+}
---
+++ ch14_람다식/src/andthen_compose/ConsumerAndThenEx.java
@@ -0,0 +1,19 @@
+package andthen_compose;
+
+import java.util.function.Consumer;
+
+public class ConsumerAndThenEx {
+  public static void main(String[] args) {
+    Consumer<Member> consumerA = (member) -> {
+      System.out.println(member.getName());
+    };
+    Consumer<Member> consumerB = (member) -> {
+      System.out.println(member.getId());
+    };
+
+    Consumer<Member> consumerAB = consumerA.andThen(consumerB);
+    consumerAB.accept(
+        new Member("김남준", "njkim", new Address("korea", "seoul"))
+    );
+  }
+}
