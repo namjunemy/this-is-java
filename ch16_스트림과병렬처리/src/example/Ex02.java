73b1b6157f5f516730af5dc9760836a6290a780a
Namjun Kim <namjunemy@gmail.com>
2018-01-09 03:27:57 +0900

    스트림과 병렬처리 확인문제 2,3,4

---
+++ ch16_스트림과병렬처리/src/example/Ex02.java
@@ -0,0 +1,15 @@
+package example;
+
+public class Ex02 {
+  /**
+   * 02. 스트림을 얻을 수 있는 소스가 아닌 것은 무엇입니까?
+   *
+   * 1. 컬렉션(List)
+   * 2. int, long, double 범위
+   * 3. 디렉토리
+   * 4. 배열
+   *
+   * 4. -> int, long의 범위만 가능하다. double은 범위가 너무 크다.
+   *
+   */
+}
